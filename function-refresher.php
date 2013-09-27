aac32126b642826530ba59ba54962f9760470f27
Your Name Here <amyr@amyrhaq.com>
2013-09-26 17:43:17 -0700

    completed lesson 1/7

---
+++ function-refresher.php
@@ -0,0 +1,14 @@
+<!-- Let's do a little review. Use the strlen function to echo the number of characters  in amyr -->
+<html>
+    <head>
+		<title></title>
+	</head>
+	<body>
+      <p>
+        <?php
+        $name = strlen('amyr');
+        echo $name;
+        ?>
+      </p>
+    </body>
+</html>
