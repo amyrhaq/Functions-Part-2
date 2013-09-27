03b5ba0c1a38bd6e194344c3cef04fa23e11914b
Your Name Here <amyr@amyrhaq.com>
2013-09-26 17:57:57 -0700

    completed lesson 3/7

---
+++ your-first-function.php
@@ -0,0 +1,18 @@
+<!-- write a function called displayName, that takes no parameters and will echo a string that contains your name. -->
+<html>
+	<head>
+		<title>Your First Function</title>
+	</head>
+	<body>
+      <p>
+        <?php
+        // Write your function below!
+        function displayName() {
+            echo $name;
+        }
+
+        
+        ?>
+      </p>
+    </body>
+</html>
