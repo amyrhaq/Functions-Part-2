abb5d775765f51ff600d12809b610bf0f21e3656
Your Name Here <amyr@amyrhaq.com>
2013-09-26 17:51:03 -0700

    completed lesson 2/7

---
+++ function-syntax.php
@@ -0,0 +1,19 @@
+<!-- Take a look at the helloWorld() function below. It takes no parameters, and when called it will simply print the string "Hello world!". -->
+<html>
+    <head>
+		<title></title>
+	</head>
+	<body>
+      <p>
+        <?php
+          // Here we define the function...
+          function helloWorld() {
+            echo "Hello world!";
+          }
+          
+          // ...and here we call it!
+          helloWorld();
+        ?>
+      </p>
+    </body>
+</html>
